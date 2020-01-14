264b624c52f872c56d5dfff8f6f59dcd43b4a5b9
dragon <dragonishare@gmail.com>
2020-01-15 00:05:35 +0800

    页面布局修改

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,7 +13,7 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+  /*   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -21,27 +21,23 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `) */
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
+    <div className="wrapper">
+      <Header />
+      <main className="main">
+        <aside className="sidebar">
+          <div className="sidebar-inner"></div>
+        </aside>
+        <article className="content">{children}</article>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
-    </>
+      </main>
+    </div>
   )
 }
 
