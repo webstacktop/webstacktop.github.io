99634b427a75975df6a6632dee4522f69d667d77
Dragon <dragonishare@gmail.com>
2020-02-11 22:02:53 +0800

    fixbug route path error

--- src/components/header.js
+++ src/components/header.js
@@ -9,22 +9,22 @@ const Header = ({ siteTitle }) => (
         webstack<b>Top</b>
       </Link>
       <nav className="nav">
-        <Link to="/docs-html-css/basics/basics" className="nav-item html">
+        <Link to="/html-css/basics/basics" className="nav-item html">
           HTML5/CSS3
         </Link>
         <Link
-          to="/docs-javascript/basics/basics"
+          to="/javascript/basics/basics"
           className="nav-item javascript"
         >
           JavaScript
         </Link>
-        <Link to="/docs-nodejs/basics/basics" className="nav-item nodejs">
+        <Link to="/nodejs/basics/basics" className="nav-item nodejs">
           Node.js
         </Link>
-        <Link to="/docs-reactjs" className="nav-item reactjs">
+        <Link to="/reactjs" className="nav-item reactjs">
           React.js
         </Link>
-        <Link to="/docs-git" className="nav-item git">
+        <Link to="/git" className="nav-item git">
           Git
         </Link>
         <Link to="/blog" className="nav-item blog">
